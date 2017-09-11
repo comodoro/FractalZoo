aeb9a10e29096187b190794d4a44c184b87c0fe5
Vojta Drabek <vojta.drabek@gmail.com>
2017-09-11 17:18:50 +0200

    Remove settings.json, changed list icon, added Utils

---
+++ app/src/main/java/com/draabek/fractal/Utils.java
@@ -0,0 +1,12 @@
+package com.draabek.fractal;
+
+/**
+ * Created by Vojta on 11.09.2017.
+ */
+
+public class Utils {
+    public static final String FRACTALS_PREFERENCE	= "FRACTALS_PREFERENCE";
+    public static final String PREFS_CURRENT_FRACTAL_KEY = "prefs_current_fractal";
+    public static final boolean DEBUG = true;
+
+}
