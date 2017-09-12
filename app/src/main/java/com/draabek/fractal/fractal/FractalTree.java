0c4aa07e620c96220f2d2302a64f89e40e4c1267
Vojta Drabek <vojta.drabek@gmail.com>
2017-09-12 21:41:26 +0200

    Add fractal tree

---
+++ app/src/main/java/com/draabek/fractal/fractal/FractalTree.java
@@ -0,0 +1,42 @@
+package com.draabek.fractal.fractal;
+
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+
+/**
+ * Created by Vojta on 12.09.2017.
+ * Based on https://rosettacode.org/wiki/Fractal_tree#Java
+ */
+
+public class FractalTree extends CanvasFractal {
+    @Override
+    public void draw(Canvas canvas) {
+        int iterations = this.getSettings().get("iterations").intValue();
+        float centerX = this.getSettings().get("centerX");
+        float centerY = this.getSettings().get("centerY");
+        float angleInc = this.getSettings().get("angle");
+        Paint paint = new Paint();
+        paint.setColor(Color.BLACK);
+        paint.setStyle(Paint.Style.FILL);
+        canvas.drawRect(0, 0, canvas.getWidth(), canvas.getHeight(), paint);
+        paint.setColor(Color.WHITE);
+        paint.setStyle(Paint.Style.STROKE);
+        paint.setStrokeWidth(1.0f);
+        int x1 = (int)(canvas.getWidth() / 2 + centerX * canvas.getWidth());
+        int y1 = (int)(centerY * canvas.getHeight());
+        drawTree(canvas, paint, x1, y1, -90, iterations);
+    }
+
+    private void drawTree(Canvas canvas, Paint paint, int x1, int y1, float angle, int depth) {
+        if (depth == 0) return;
+        int iterations = this.getSettings().get("iterations").intValue();
+        int x2 = x1 + (int) (Math.cos(Math.toRadians(angle)) * depth * 50.0/iterations);
+        int y2 = y1 + (int) (Math.sin(Math.toRadians(angle)) * depth * 50.0/iterations);
+        canvas.drawLine(x1, y1, x2, y2, paint);
+        float angleInc = this.getSettings().get("angle");
+        drawTree(canvas, paint, x2, y2, angle - angleInc, depth - 1);
+        drawTree(canvas, paint, x2, y2, angle + angleInc, depth - 1);
+    }
+
+}
