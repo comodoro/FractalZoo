acff965bcaee8e3876a8a4b0e328b444350be8a4
Vojta Drabek <vojta.drabek@gmail.com>
2017-09-25 20:40:47 +0200

    Saving bitmaps including weird Marshmallow permissions

--- app/src/main/java/com/draabek/fractal/SaveBitmapActivity.java
+++ app/src/main/java/com/draabek/fractal/SaveBitmapActivity.java
@@ -1,21 +1,27 @@
 package com.draabek.fractal;
 
+import android.Manifest;
 import android.app.WallpaperManager;
+import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
+import android.os.Handler;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioGroup;
+import android.widget.Toast;
 
 import com.draabek.fractal.fractal.FractalRegistry;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.channels.FileChannel;
@@ -23,13 +29,15 @@ import java.nio.channels.FileChannel;
 /**
  * A login screen that offers login via email/password.
  */
-public class SaveBitmapActivity extends AppCompatActivity {
+public class SaveBitmapActivity extends AppCompatActivity implements ActivityCompat.OnRequestPermissionsResultCallback{
 
     private static final String LOG_KEY = SaveBitmapActivity.class.getName();
+    private static final int REQUEST_WRITE = 1;
+    private static final int REQUEST_WALLPAPER = 2;
+
     // UI references.
     private RadioGroup radioGroup;
     EditText filenameEdit;
-    private Button button;
     private File bitmapFile;
 
     @Override
@@ -38,7 +46,7 @@ public class SaveBitmapActivity extends AppCompatActivity {
         setContentView(R.layout.activity_save_bitmap);
         radioGroup = (RadioGroup) findViewById(R.id.save_bitmap_radio_group);
         filenameEdit = (EditText) findViewById(R.id.bitmap_filename);
-        button = (Button) findViewById(R.id.save_bitmap_ok_button);
+        Button button = (Button) findViewById(R.id.save_bitmap_ok_button);
         bitmapFile = new File(this.getIntent().getStringExtra(getString(R.string.intent_extra_bitmap_file)));
         File suggestedPath = getFile();
         filenameEdit.getText().append(suggestedPath.getAbsolutePath());
@@ -46,23 +54,87 @@ public class SaveBitmapActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 if (radioGroup.getCheckedRadioButtonId() == R.id.bitmap_filename_radio) {
-                    String filename = filenameEdit.getText().toString();
-                    File f = new File(filename);
-                    saveBitmap(bitmapFile, f);
-                    finish();
-                } else if (radioGroup.getCheckedRadioButtonId() == R.id.bitmap_set_as_background_radio) {
-                    WallpaperManager myWallpaperManager = WallpaperManager.getInstance(SaveBitmapActivity.this);
-                    try {
-                        myWallpaperManager.setStream(new FileInputStream(bitmapFile));
-                    } catch (IOException e) {
-                        // Tjust be ugly in the logcat
-                        e.printStackTrace();
+                    if (handlePermissions(Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                            getString(R.string.save_bitmap_storage_write_rationale), REQUEST_WRITE)) {
+                        saveToFile();
                     }
+                } else if (radioGroup.getCheckedRadioButtonId() == R.id.bitmap_set_as_background_radio) {
+                   // handlePermissions(Manifest.permission.SET_WALLPAPER,
+                     //       getString(R.string.save_bitmap_set_wallpaper_rationale), REQUEST_WALLPAPER);
+                    setAsWallpaper();
                 } else {
                     Log.e(this.getClass().getName(), "Unknown radio button in SaveBitmapActivity");
                 }
             }
         });
+    }
+
+    private void saveToFile() {
+        String filename = filenameEdit.getText().toString();
+        File f = new File(filename);
+        saveBitmap(bitmapFile, f);
+        Toast.makeText(SaveBitmapActivity.this, getString(R.string.save_bitmap_success_toast)
+                        + bitmapFile.getAbsolutePath(),
+                Toast.LENGTH_LONG).show();
+        finish();
+    }
+
+    private void setAsWallpaper() {
+        WallpaperManager myWallpaperManager = WallpaperManager.getInstance(SaveBitmapActivity.this);
+        try {
+            myWallpaperManager.setStream(new FileInputStream(bitmapFile));
+        } catch (IOException e) {
+            // Just be ugly in the logcat
+            e.printStackTrace();
+            finish();
+        }
+        finish();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+
+        if (requestCode == REQUEST_WRITE) {
+            if (grantResults.length == 1 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                saveToFile();
+            } else {
+                Toast.makeText(SaveBitmapActivity.this, getString(R.string.save_bitmap_storage_write_rationale),
+                        Toast.LENGTH_SHORT).show();
+            }
+        } else if (requestCode == REQUEST_WALLPAPER) {
+            if (grantResults.length == 1 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                setAsWallpaper();
+            } else {
+                Toast.makeText(SaveBitmapActivity.this, getString(R.string.save_bitmap_set_wallpaper_rationale),
+                        Toast.LENGTH_SHORT).show();
+            }
+        }
+        finish();
+    }
+
+    private boolean handlePermissions(final String permission, final String rationale, final int code) {
+        if (ContextCompat.checkSelfPermission(SaveBitmapActivity.this, permission)
+                != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(SaveBitmapActivity.this,permission)) {
+                //Just display a Toast
+                Toast.makeText(SaveBitmapActivity.this, rationale, Toast.LENGTH_SHORT).show();
+                final Handler handler = new Handler();
+                handler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        ActivityCompat.requestPermissions(SaveBitmapActivity.this,
+                                new String[]{permission}, code);
+                    }
+                }, Toast.LENGTH_SHORT);
+            } else {
+                // No explanation needed, we can request the permission.
+                ActivityCompat.requestPermissions(SaveBitmapActivity.this,
+                        new String[]{permission}, code);
+            }
+            return false;
+        }
+        return true;
     }
 
     private boolean saveBitmap(File bitmapTempFile, File path) {
@@ -73,14 +145,16 @@ public class SaveBitmapActivity extends AppCompatActivity {
             destChannel = new FileOutputStream(path).getChannel();
             destChannel.transferFrom(sourceChannel, 0, sourceChannel.size());
             return true;
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         } finally{
             try {
-                sourceChannel.close();
-                destChannel.close();
+                if (sourceChannel != null) {
+                    sourceChannel.close();
+                }
+                if (destChannel != null) {
+                    destChannel.close();
+                }
             } catch (IOException e) {
                 e.printStackTrace();
             }
