3c52f99f2a194255a1bdd9ec3f0828586e66b575
Vojta Drabek <vojta.drabek@gmail.com>
2017-10-14 15:04:35 +0200

    Working simple test

--- app/src/androidTest/java/com/draabek/fractal/MainActivityTest.java
+++ app/src/androidTest/java/com/draabek/fractal/MainActivityTest.java
@@ -4,7 +4,6 @@ package com.draabek.fractal;
 import android.support.test.espresso.ViewInteraction;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
-import android.test.suitebuilder.annotation.LargeTest;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewParent;
@@ -17,52 +16,63 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static android.support.test.espresso.Espresso.onView;
-import static android.support.test.espresso.Espresso.pressBack;
 import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static android.support.test.espresso.action.ViewActions.replaceText;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
-import static android.support.test.espresso.matcher.ViewMatchers.withClassName;
 import static android.support.test.espresso.matcher.ViewMatchers.withContentDescription;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withParent;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static org.hamcrest.Matchers.allOf;
-import static org.hamcrest.Matchers.is;
 
-@LargeTest
+
 @RunWith(AndroidJUnit4.class)
-public class FirstEspressoTest {
+public class MainActivityTest {
 
     @Rule
     public ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule<>(MainActivity.class);
 
     @Test
-    public void firstEspressoTest() {
+    public void mainActivityTest() {
         ViewInteraction actionMenuItemView = onView(
                 allOf(withId(R.id.fractalList), withContentDescription("Fractal List"), isDisplayed()));
         actionMenuItemView.perform(click());
 
         ViewInteraction textView = onView(
-                allOf(withId(android.R.id.text1), withText("Mandelbrot"),
+                allOf(withId(android.R.id.text1), withText("Julia"),
                         childAtPosition(
                                 allOf(withId(android.R.id.list),
                                         withParent(withId(android.R.id.content))),
-                                2),
+                                6),
                         isDisplayed()));
         textView.perform(click());
 
         ViewInteraction actionMenuItemView2 = onView(
-                allOf(withId(R.id.options), withContentDescription("Options"), isDisplayed()));
+                allOf(withId(R.id.save), withContentDescription("Save as JPG"), isDisplayed()));
         actionMenuItemView2.perform(click());
 
-        ViewInteraction linearLayout = onView(
-                allOf(childAtPosition(
-                        allOf(withId(android.R.id.list),
-                                withParent(withClassName(is("android.widget.LinearLayout")))),
-                        0),
+        ViewInteraction appCompatEditText = onView(
+                allOf(withId(R.id.bitmap_filename),
+                        withParent(withId(R.id.save_bitmap_radio_group)),
                         isDisplayed()));
-        linearLayout.perform(click());
+        appCompatEditText.perform(replaceText("/storage/emulated/0/Pictures/Julia1507985989003.jpg"), closeSoftKeyboard());
 
-        pressBack();
+        ViewInteraction appCompatEditText2 = onView(
+                allOf(withId(R.id.bitmap_filename), withText("/storage/emulated/0/Pictures/Julia1507985989003.jpg"),
+                        withParent(withId(R.id.save_bitmap_radio_group)),
+                        isDisplayed()));
+        appCompatEditText2.perform(click());
+
+        ViewInteraction appCompatRadioButton = onView(
+                allOf(withId(R.id.bitmap_set_as_background_radio), withText("Set as Home screen background"),
+                        withParent(withId(R.id.save_bitmap_radio_group)),
+                        isDisplayed()));
+        appCompatRadioButton.perform(click());
+
+        ViewInteraction appCompatButton = onView(
+                allOf(withId(R.id.save_bitmap_ok_button), withText("OK"), isDisplayed()));
+        appCompatButton.perform(click());
 
     }
 
