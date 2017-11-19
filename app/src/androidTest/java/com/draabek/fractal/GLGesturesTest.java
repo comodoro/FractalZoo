6c770c08343985c884424f6a44deba1f4b55415a
Vojta Drabek <vojta.drabek@gmail.com>
2017-11-19 22:08:25 +0100

    Modified Travis and tests

--- app/src/androidTest/java/com/draabek/fractal/GLGesturesTest.java
+++ app/src/androidTest/java/com/draabek/fractal/GLGesturesTest.java
@@ -1,8 +1,11 @@
 package com.draabek.fractal;
 
 
-import android.support.test.espresso.DataInteraction;
-import android.support.test.espresso.ViewInteraction;
+import android.support.test.espresso.ViewAction;
+import android.support.test.espresso.action.GeneralLocation;
+import android.support.test.espresso.action.GeneralSwipeAction;
+import android.support.test.espresso.action.Press;
+import android.support.test.espresso.action.Swipe;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 import android.test.suitebuilder.annotation.LargeTest;
@@ -17,20 +20,6 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static android.support.test.espresso.Espresso.onData;
-import static android.support.test.espresso.Espresso.onView;
-import static android.support.test.espresso.action.ViewActions.click;
-import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
-import static android.support.test.espresso.action.ViewActions.replaceText;
-import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
-import static android.support.test.espresso.matcher.ViewMatchers.withClassName;
-import static android.support.test.espresso.matcher.ViewMatchers.withContentDescription;
-import static android.support.test.espresso.matcher.ViewMatchers.withId;
-import static android.support.test.espresso.matcher.ViewMatchers.withText;
-import static org.hamcrest.Matchers.allOf;
-import static org.hamcrest.Matchers.anything;
-import static org.hamcrest.Matchers.is;
-
 @LargeTest
 @RunWith(AndroidJUnit4.class)
 public class GLGesturesTest {
@@ -40,93 +29,18 @@ public class GLGesturesTest {
 
     @Test
     public void gLGesturesTest() {
-        ViewInteraction actionMenuItemView = onView(
-                allOf(withId(R.id.fractalList), withContentDescription("Fractal List"),
-                        isDisplayed()));
-        actionMenuItemView.perform(click());
+        swipeUp();
+        swipeDown();
+    }
 
-        DataInteraction textView = onData(anything())
-                .inAdapterView(allOf(withId(android.R.id.list),
-                        childAtPosition(
-                                withId(android.R.id.content),
-                                0)))
-                .atPosition(1);
-        textView.perform(click());
+    public static ViewAction swipeUp() {
+        return new GeneralSwipeAction(Swipe.FAST, GeneralLocation.BOTTOM_CENTER,
+                GeneralLocation.TOP_CENTER, Press.FINGER);
+    }
 
-        ViewInteraction actionMenuItemView2 = onView(
-                allOf(withId(R.id.save), withContentDescription("Save as JPG"),
-                        childAtPosition(
-                                childAtPosition(
-                                        withId(R.id.toolbar),
-                                        0),
-                                1),
-                        isDisplayed()));
-        actionMenuItemView2.perform(click());
-
-        ViewInteraction appCompatEditText = onView(
-                allOf(withId(R.id.bitmap_filename),
-                        childAtPosition(
-                                allOf(withId(R.id.save_bitmap_radio_group),
-                                        childAtPosition(
-                                                withClassName(is("android.widget.LinearLayout")),
-                                                0)),
-                                1),
-                        isDisplayed()));
-        appCompatEditText.perform(replaceText("/storage/emulated/0/Pictures/Mandelbrot1509044038915.jpg"), closeSoftKeyboard());
-
-        ViewInteraction appCompatRadioButton = onView(
-                allOf(withId(R.id.bitmap_filename_radio), withText("Save as file"),
-                        childAtPosition(
-                                allOf(withId(R.id.save_bitmap_radio_group),
-                                        childAtPosition(
-                                                withClassName(is("android.widget.LinearLayout")),
-                                                0)),
-                                0),
-                        isDisplayed()));
-        appCompatRadioButton.perform(click());
-
-        ViewInteraction appCompatEditText2 = onView(
-                allOf(withId(R.id.bitmap_filename), withText("/storage/emulated/0/Pictures/Mandelbrot1509044038915.jpg"),
-                        childAtPosition(
-                                allOf(withId(R.id.save_bitmap_radio_group),
-                                        childAtPosition(
-                                                withClassName(is("android.widget.LinearLayout")),
-                                                0)),
-                                1),
-                        isDisplayed()));
-        appCompatEditText2.perform(click());
-
-        ViewInteraction appCompatButton = onView(
-                allOf(withId(R.id.save_bitmap_ok_button), withText("OK"),
-                        childAtPosition(
-                                childAtPosition(
-                                        withId(android.R.id.content),
-                                        0),
-                                1),
-                        isDisplayed()));
-        appCompatButton.perform(click());
-
-        ViewInteraction actionMenuItemView3 = onView(
-                allOf(withId(R.id.save), withContentDescription("Save as JPG"),
-                        childAtPosition(
-                                childAtPosition(
-                                        withId(R.id.toolbar),
-                                        0),
-                                1),
-                        isDisplayed()));
-        actionMenuItemView3.perform(click());
-
-        ViewInteraction appCompatEditText3 = onView(
-                allOf(withId(R.id.bitmap_filename),
-                        childAtPosition(
-                                allOf(withId(R.id.save_bitmap_radio_group),
-                                        childAtPosition(
-                                                withClassName(is("android.widget.LinearLayout")),
-                                                0)),
-                                1),
-                        isDisplayed()));
-        appCompatEditText3.perform(replaceText("/storage/emulated/0/Pictures/Mandelbrot1509044146491.jpg"), closeSoftKeyboard());
-
+    public static ViewAction swipeDown() {
+        return new GeneralSwipeAction(Swipe.FAST, GeneralLocation.BOTTOM_CENTER,
+                GeneralLocation.BOTTOM_CENTER, Press.FINGER);
     }
 
     private static Matcher<View> childAtPosition(
--- app/src/androidTest/java/com/draabek/fractal/MainActivityTest.java
+++ app/src/androidTest/java/com/draabek/fractal/MainActivityTest.java
@@ -1,9 +1,11 @@
 package com.draabek.fractal;
 
 
+import android.support.test.espresso.DataInteraction;
 import android.support.test.espresso.ViewInteraction;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
+import android.test.suitebuilder.annotation.LargeTest;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewParent;
@@ -15,18 +17,16 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static android.support.test.espresso.Espresso.onData;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
-import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
-import static android.support.test.espresso.action.ViewActions.replaceText;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withContentDescription;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
-import static android.support.test.espresso.matcher.ViewMatchers.withParent;
-import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.anything;
 
-
+@LargeTest
 @RunWith(AndroidJUnit4.class)
 public class MainActivityTest {
 
@@ -36,43 +36,58 @@ public class MainActivityTest {
     @Test
     public void mainActivityTest() {
         ViewInteraction actionMenuItemView = onView(
-                allOf(withId(R.id.fractalList), withContentDescription("Fractal List"), isDisplayed()));
+                allOf(withId(R.id.fractalList), withContentDescription("Fractal List"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(R.id.toolbar),
+                                        0),
+                                0),
+                        isDisplayed()));
         actionMenuItemView.perform(click());
 
-        ViewInteraction textView = onView(
-                allOf(withId(android.R.id.text1), withText("Julia"),
+        DataInteraction relativeLayout = onData(anything())
+                .inAdapterView(allOf(withId(android.R.id.list),
                         childAtPosition(
-                                allOf(withId(android.R.id.list),
-                                        withParent(withId(android.R.id.content))),
-                                6),
-                        isDisplayed()));
-        textView.perform(click());
+                                withId(android.R.id.content),
+                                0)))
+                .atPosition(0);
+        relativeLayout.perform(click());
 
         ViewInteraction actionMenuItemView2 = onView(
-                allOf(withId(R.id.save), withContentDescription("Save as JPG"), isDisplayed()));
+                allOf(withId(R.id.fractalList), withContentDescription("Fractal List"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(R.id.toolbar),
+                                        0),
+                                0),
+                        isDisplayed()));
         actionMenuItemView2.perform(click());
 
-        ViewInteraction appCompatEditText = onView(
-                allOf(withId(R.id.bitmap_filename),
-                        withParent(withId(R.id.save_bitmap_radio_group)),
-                        isDisplayed()));
-        appCompatEditText.perform(replaceText("/storage/emulated/0/Pictures/Julia1507985989003.jpg"), closeSoftKeyboard());
+        DataInteraction relativeLayout2 = onData(anything())
+                .inAdapterView(allOf(withId(android.R.id.list),
+                        childAtPosition(
+                                withId(android.R.id.content),
+                                0)))
+                .atPosition(1);
+        relativeLayout2.perform(click());
 
-        ViewInteraction appCompatEditText2 = onView(
-                allOf(withId(R.id.bitmap_filename), withText("/storage/emulated/0/Pictures/Julia1507985989003.jpg"),
-                        withParent(withId(R.id.save_bitmap_radio_group)),
+        ViewInteraction actionMenuItemView3 = onView(
+                allOf(withId(R.id.fractalList), withContentDescription("Fractal List"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(R.id.toolbar),
+                                        0),
+                                0),
                         isDisplayed()));
-        appCompatEditText2.perform(click());
+        actionMenuItemView3.perform(click());
 
-        ViewInteraction appCompatRadioButton = onView(
-                allOf(withId(R.id.bitmap_set_as_background_radio), withText("Set as Home screen background"),
-                        withParent(withId(R.id.save_bitmap_radio_group)),
-                        isDisplayed()));
-        appCompatRadioButton.perform(click());
-
-        ViewInteraction appCompatButton = onView(
-                allOf(withId(R.id.save_bitmap_ok_button), withText("OK"), isDisplayed()));
-        appCompatButton.perform(click());
+        DataInteraction relativeLayout3 = onData(anything())
+                .inAdapterView(allOf(withId(android.R.id.list),
+                        childAtPosition(
+                                withId(android.R.id.content),
+                                0)))
+                .atPosition(2);
+        relativeLayout3.perform(click());
 
     }
 
